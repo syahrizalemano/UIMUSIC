41f70488045c542c0a50c2585ac3511ce8dfc3b0
syahrizalemano <89107294+syahrizalemano@users.noreply.github.com>
2021-09-12 12:32:00 +0800

    Update channelplay.py

--- GeezProject/modules/channelplay.py
+++ GeezProject/modules/channelplay.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from node-fetch import fetch
+import fetch from 'node-fetch';
 
 import json
 import os
