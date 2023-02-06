5058054f20adc05e0624be639f65540c74273d1d
Hunter-XD <85411336+Hunter-XD@users.noreply.github.com>
2023-02-06 14:02:19 +0000

    PyLint Auto Fixes

--- GeezProject/__main__.py
+++ GeezProject/__main__.py
@@ -1,8 +1,9 @@
 import requests
-from GeezProject.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from GeezProject.services.pytgcalls import run
 from pyrogram import Client as Bot
 from pytgcalls import idle
+
+from GeezProject.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from GeezProject.services.pytgcalls import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
