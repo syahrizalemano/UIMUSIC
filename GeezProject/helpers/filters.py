4140f66dea1d6fdc08d1beed336b73c6144d4fc0
Anobots <89107294+syahrizalemano@users.noreply.github.com>
2023-02-13 11:35:15 +0800

    Update filters.py

--- GeezProject/helpers/filters.py
+++ GeezProject/helpers/filters.py
@@ -22,10 +22,10 @@ from pyrogram import filters
 from GeezProject.config import COMMAND_PREFIXES
 
 other_filters = (
-    filters.group & ~on_edited_message & ~filters.via_bot & ~filters.forwarded
+    filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 )
 other_filters2 = (
-    filters.private & ~on_edited_message & ~filters.via_bot & ~filters.forwarded
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 )
 
 
